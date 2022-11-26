c336db082d9cb26c112bd3b8ca779c6620a16e4c
topgirl2410 <maria.topgirl82@gmail.com>
2022-11-26 23:36:14 +0100

    finalizando el Header

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-const { src, dest, watch } = require("gulp")
+const { src, dest, watch } = require("gulp");
+const plumber = require('gulp-plumber');
 const sass = require('gulp-sass')(require('sass'));
+
 
 function css(done) {
     src('src/scss/**/*.scss') // Identificar el archivo de SASS 
+        .pipe(plumber())
         .pipe(sass()) // Compilar
         .pipe(dest('build/css')) // Almacenar en el disco duro
 
