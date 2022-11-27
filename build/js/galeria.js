fef36d138185a1d1ca98069b64e689c0eb1ee0f9
topgirl2410 <maria.topgirl82@gmail.com>
2022-11-27 22:45:21 +0100

    añadir un boton para cerrar la imagen

--- build/js/galeria.js
+++ build/js/galeria.js
@@ -19,6 +19,45 @@ function crearGaleria() {
                     alt="Imagen Galería">
         `;
 
+        imagen.onclick = function () {
+            mostrarImagen(i);
+        }
+
         galeria.appendChild(imagen)
     }
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+                <source srcset="build/img/grande/${id}.avif" type="image/avif">
+                <source srcset="build/img/grande/${id}.webp" type="image/webp">
+                <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg"
+                    alt="Imagen Galería">
+        `;
+    // Crea el Overlay con la imagen 
+
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+
+    // Boton para cerrar el Modal
+
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.onclick = function() {
+
+
+        overlay.remove();
+    }
+    cerrarModal.classList.add('btn-cerrar');
+
+
+    overlay.appendChild(cerrarModal)
+
+    // Añadirlo al HTML 
+
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+
 }
