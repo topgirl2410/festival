cca4ebfb0244a744e248ebfd5b718d3a827b7937
topgirl2410 <maria.topgirl82@gmail.com>
2022-11-27 15:55:00 +0100

    finalizando el SCSS al lineup

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ function imagenes(done) {
         optimizationLevel: 3,
     }
 
-    src('src/img/**/*.{jpg, png}')
+    src('src/img/**/*.{png, jpg}')
         .pipe(cache(imagemin(opciones)))
         .pipe(dest('build/img'))
 
@@ -39,7 +39,7 @@ function versionWebp(done) {
         quality: 50
     };
 
-    src('src/img/**/*.{jpg, png}')
+    src('src/img/**/*.{png, jpg}')
         .pipe(webp(opciones))
         .pipe(dest('build/img'))
 
@@ -52,7 +52,7 @@ function versionAvif(done) {
         quality: 50
     };
 
-    src('src/img/**/*.{jpg, png}')
+    src('src/img/**/*.{png, jpg}')
         .pipe(avif(opciones))
         .pipe(dest('build/img'))
 
