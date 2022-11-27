f87828d3dbe79830c43fc771c6e2597cbad9eda9
topgirl2410 <maria.topgirl82@gmail.com>
2022-11-27 13:54:53 +0100

    creando imagenes avif

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ const sass = require('gulp-sass')(require('sass'));
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 
 function css(done) {
@@ -45,6 +46,19 @@ function versionWebp(done) {
     done();
 }
 
+
+function versionAvif(done) {
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{jpg, png}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+
+    done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css)
     done();
@@ -53,4 +67,5 @@ function dev(done) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes, versionWebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
