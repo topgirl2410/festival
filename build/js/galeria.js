0ad487f0945cdf877876a6cbb25edc19ac0df6e3
topgirl2410 <maria.topgirl82@gmail.com>
2022-12-01 14:10:57 +0100

    detectar el scroll con JS

--- build/js/galeria.js
+++ build/js/galeria.js
@@ -3,8 +3,36 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 function iniciarApp() {
+    navegacionFija()
     crearGaleria();
+    scrollNav();
+}
 
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+
+    window.addEventListener('scroll', function () {
+
+        if (sobreFestival.getBoundingClientRect().top < 0) {
+            barra.classList.add('fijo');
+        } else {
+            barra.classList.remove('fijo');
+        }
+    });
+}
+
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: 'smooth' });
+        })
+    })
 }
 
 function crearGaleria() {
